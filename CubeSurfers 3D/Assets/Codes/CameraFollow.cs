6967144ce42dd092424b990d6142eeced4c6327f
alfonsoquartny <81421037+alfonsoquartny@users.noreply.github.com>
2022-03-12 17:05:26 +0300

    Started Project
    Default Company

---
+++ CubeSurfers 3D/Assets/Codes/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject hedef;
+    public Vector3 mesafe;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, hedef.transform.position + mesafe, 0.25f);  
+    }
+}
