bf778a38712b30948f420ec07839667d1cdf3934
alfonsoquartny <81421037+alfonsoquartny@users.noreply.github.com>
2022-03-12 17:58:46 +0300

    Evulation Mechanic
    a

--- Cube Surfer alp/Assets/EvulationScript.cs
+++ Cube Surfer alp/Assets/EvulationScript.cs
@@ -11,6 +11,11 @@ public class EvulationScript : MonoBehaviour
     public Mesh Level1Mesh;
     public Mesh Level2Mesh;
     public Mesh Level3Mesh;
+    public Mesh Level4Mesh;
+    public Mesh Level5Mesh;
+    public Mesh Level6Mesh;
+    public Mesh Level7Mesh;
+    public Mesh Level8Mesh;
 
     public MeshFilter meshFilter;
 
@@ -26,6 +31,7 @@ public class EvulationScript : MonoBehaviour
     {
         if (activeLevel == 1)
         {
+            
             meshFilter.mesh = Level1Mesh;
         }
         if (activeLevel == 2)
@@ -37,6 +43,29 @@ public class EvulationScript : MonoBehaviour
         {
             meshFilter.mesh = Level3Mesh;
         }
+        if (activeLevel == 4)
+        {
+            meshFilter.mesh = Level4Mesh;
+        }
+        if (activeLevel == 5)
+        {
+            meshFilter.mesh = Level5Mesh;
+        }
+        if (activeLevel == 6)
+        {
+            meshFilter.mesh = Level6Mesh;
+        }
+        if (activeLevel == 7)
+        {
+            meshFilter.mesh = Level7Mesh;
+        }
+      
+        if (activeLevel == 8)
+        {
+            meshFilter.mesh = Level8Mesh;
+        }
+
+
 
         if (isTemas == true && activeLevel < maxLevel)
         {
